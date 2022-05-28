4db9122ed60082368a3dd25cf096c3f731d65bd7
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-05-29 00:19:51 +0530

    main file updated

---
+++ balloon/src/Components/Main.jsx
@@ -0,0 +1,130 @@
+import React, { useEffect, useState } from "react";
+import "./Main.css";
+
+export default function Main() {
+  // --------------data initialise ---------------
+  const circle = [
+    {
+      color: "blue",
+      index: 1,
+    },
+    {
+      color: "brown",
+      index: 2,
+    },
+    {
+      color: "green",
+      index: 3,
+    },
+    {
+      color: "pink",
+      index: 4,
+    },
+    {
+      color: "cyan",
+      index: 5,
+    },
+  ];
+
+  // -----------------  useState initilise --------------
+
+  const [Circle, setCircle] = useState([]);
+  const [shootnum, setShootnum] = useState("");
+  const [emptyDiv, setEmptyDiv] = useState([]);
+
+  useEffect(() => {
+    setCircle(circle);
+  }, []);
+
+  // ------------------ sort the input -----------------------
+
+  Circle.sort((x, y) => x.index - y.index);
+
+  //   onclick buttion event shoot button
+  const shootChange = () => {
+    if (Circle[shootnum - 1]) {
+      setEmptyDiv([...emptyDiv, Circle[shootnum - 1]]);
+
+      const afteradd = Circle.filter(
+        (e) => Circle[shootnum - 1].index !== e.index
+      );
+      setCircle(afteradd);
+      setShootnum("")
+    } else {
+      alert("Enter Valid Number");
+    }
+  };
+
+  //  ------------------- onclick circle event -----------------------
+
+  const onclickingCircle = (e) => {
+    setCircle([...Circle, e]);
+    const filterData = emptyDiv.filter((x) => e.index !== x.index);
+    setEmptyDiv(filterData);
+  };
+
+  return (
+    <div className="HomeMain">
+      {/* input and button */}
+      <div className="InputMainDiv">
+
+          {/* -------input box to shoot the no. --------- */}
+        <input
+          type="text"
+          placeholder=" Type Number to shoot "
+          value={shootnum}
+          onChange={(e) => setShootnum(e.target.value)}
+        />
+        <br /> <br />
+        <button onClick={() => shootChange()}>Shoot</button>
+      </div>
+
+      
+      <div className="MainDiv">
+
+        {/* --------------empty circle div ----------------*/}
+
+        
+        <div className="Empty_Div">
+        <p>Empty Div</p>
+
+
+          {/* data append */}
+
+          {emptyDiv.map((e) => (
+            <div
+              className="SingleDiv"
+              style={{ background: `${e.color}` }}
+              key={e.index}
+              onClick={() => onclickingCircle(e)}
+            >
+
+                {/* //-------- indexing circle -------- */}
+
+              {/* <p>{e.index}</p> */}
+
+            </div>
+          ))}
+        </div>
+
+        {/* circle main div of origin */}
+
+        <div>
+          {/* data append */}
+          {Circle.map((e) => (
+            <div
+              className="SingleDiv"
+              style={{ background: `${e.color}` }}
+              key={e.index}
+            >
+
+                {/* //---------- indexing circle------------ */}
+
+              {/* <p>{e.index}</p> */}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
